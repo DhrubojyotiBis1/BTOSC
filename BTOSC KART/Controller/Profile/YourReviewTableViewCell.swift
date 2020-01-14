f0b33fc52894f8727b668a930bd93b5f0f28599b
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-01-15 01:43:53 +0530

    profile UI complete

---
+++ BTOSC KART/Controller/Profile/YourReviewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  YourReviewTableViewCell.swift
+//  BTOSC KART
+//
+//  Created by Dhrubojyoti on 15/01/20.
+//  Copyright © 2020 Dhrubojyoti. All rights reserved.
+//
+
+import UIKit
+
+class YourReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
