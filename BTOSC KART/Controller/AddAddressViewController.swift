49f7f39bd38cc82784270d474ed89d8fdcc2ab53
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-01-16 01:20:47 +0530

    add address UI complete

---
+++ BTOSC KART/Controller/AddAddressViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddAddressViewController.swift
+//  BTOSC KART
+//
+//  Created by Dhrubojyoti on 15/01/20.
+//  Copyright © 2020 Dhrubojyoti. All rights reserved.
+//
+
+import UIKit
+
+class AddAddressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
