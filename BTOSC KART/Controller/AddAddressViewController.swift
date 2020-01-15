2d3ad80374a042f467f50cb68ce92ec70901bd13
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2020-01-16 01:20:56 +0530

    add address UI complete

--- BTOSC KART/Controller/AddAddressViewController.swift
+++ BTOSC KART/Controller/AddAddressViewController.swift
@@ -10,21 +10,38 @@ import UIKit
 
 class AddAddressViewController: UIViewController {
 
+    @IBOutlet var textFieldLineView: [UIView]!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet  var allTextfield:[UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.setup()
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AddAddressViewController:UITextFieldDelegate{
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let currentEditingTextFiledTag = textField.tag
+        self.textFieldLineView[currentEditingTextFiledTag].backgroundColor = custumColour.appTheamPinkColour
+        
+        for i in 0..<self.allTextfield.count{
+            if i != currentEditingTextFiledTag{
+                self.textFieldLineView[i].backgroundColor = UIColor.lightGray
+            }
+        }
     }
-    */
+}
 
+extension AddAddressViewController{
+    private func setup(){
+        
+        self.nextButton.layer.cornerRadius = 20
+        for textField in self.allTextfield{
+            textField.delegate = self
+        }
+    }
 }
